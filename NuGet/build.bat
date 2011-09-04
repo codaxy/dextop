5e722bd66221b5a01746fa22d4449107eab33ed6
Marko Stijak <mstijak@gmail.com>
2011-09-04 15:01:23 +0200

    initial symbolsource support

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,15 +1,18 @@
 REM Creating NuGet : Dextop (just the libraries js & dll)
 
+del *.nupkg
+robocopy ..\Apps\Codaxy.Dextop.Showcase\client\lib\dextop Codaxy.Dextop\content\client\lib\dextop /XD .svn /MIR
+nuget pack Codaxy.Dextop\Codaxy.Dextop.nuspec
+
 mkdir Codaxy.Dextop.Core\lib\
 copy ..\Libraries\Codaxy.Dextop\Codaxy.Dextop\bin\Release\Codaxy.Dextop.* Codaxy.Dextop.Core\lib\
-
-robocopy ..\Apps\Codaxy.Dextop.Showcase\client\lib\dextop Codaxy.Dextop\content\client\lib\dextop /XD .svn /MIR
-REM robocopy ..\Libraries\Codaxy.Dextop\sound Dextop\content\client\lib\sound /XD .svn /MIR
-
-del *.nupkg
-
-nuget pack Codaxy.Dextop\Codaxy.Dextop.nuspec
+del Codaxy.Dextop.Core\lib\*.pdb
 nuget pack Codaxy.Dextop.Core\Codaxy.Dextop.Core.nuspec
+
+mkdir Codaxy.Dextop.Core\src\
+copy ..\Libraries\Codaxy.Dextop\Codaxy.Dextop\bin\Release\Codaxy.Dextop.* Codaxy.Dextop.Core\lib\
+robocopy ..\Libraries\Codaxy.Dextop\Codaxy.Dextop Codaxy.Dextop.Core\src\ /MIR /XD obj bin
+nuget pack Codaxy.Dextop.Core\Codaxy.Dextop.Core.nuspec -Symbols
 
 REM Creating NuGet : Dextop Basic (simple app with one window, no libs)
 
