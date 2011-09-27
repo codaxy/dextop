9062e4b9817519358bc027f719ded80c7273320e
Marko Stijak <mstijak@gmail.com>
2011-09-27 09:21:10 +0200

    improved license notice part

--- Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/generated/remote.js
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/generated/remote.js
@@ -1,11 +1,13 @@
-Ext.define('Dextop.RemotableBase.remoting.Proxy', {
+Ext.define('Dextop.RemotableConfig.remoting.Proxy', {
 	extend: 'Dextop.remoting.Proxy'
 });
 
-Ext.define('Dextop.Session.remoting.Proxy', {
+Ext.define('Dextop.data.Proxy.remoting.Proxy', {
 	extend: 'Dextop.remoting.Proxy',
-	ExtendSession: function(callback, scope) { this.invokeRemoteMethod(callback, scope, 'ExtendSession', []);},
-	RegisterLookupDataVersion: function(lookupName, version, callback, scope) { this.invokeRemoteMethod(callback, scope, 'RegisterLookupDataVersion', [lookupName, version]);}
+	Load: function(optionsJSON, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Load', [optionsJSON]);},
+	Create: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Create', [json]);},
+	Update: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Update', [json]);},
+	Destroy: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Destroy', [json]);}
 });
 
 Ext.define('Dextop.data.LiveStore.remoting.Proxy', {
@@ -13,12 +15,8 @@ Ext.define('Dextop.data.LiveStore.remoting.Proxy', {
 	Subscribe: function(callback, scope) { this.invokeRemoteMethod(callback, scope, 'Subscribe', []);}
 });
 
-Ext.define('Dextop.RemotableConfig.remoting.Proxy', {
+Ext.define('Dextop.RemotableBase.remoting.Proxy', {
 	extend: 'Dextop.remoting.Proxy'
-});
-
-Ext.define('Dextop.Panel.remoting.Proxy', {
-	extend: 'Dextop.RemotableBase.remoting.Proxy'
 });
 
 Ext.define('Dextop.Window.remoting.Proxy', {
@@ -29,11 +27,13 @@ Ext.define('Dextop.Window`1.remoting.Proxy', {
 	extend: 'Dextop.Window.remoting.Proxy'
 });
 
-Ext.define('Dextop.data.Proxy.remoting.Proxy', {
+Ext.define('Dextop.Session.remoting.Proxy', {
 	extend: 'Dextop.remoting.Proxy',
-	Load: function(optionsJSON, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Load', [optionsJSON]);},
-	Create: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Create', [json]);},
-	Update: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Update', [json]);},
-	Destroy: function(json, callback, scope) { this.invokeRemoteMethod(callback, scope, 'Destroy', [json]);}
+	ExtendSession: function(callback, scope) { this.invokeRemoteMethod(callback, scope, 'ExtendSession', []);},
+	RegisterLookupDataVersion: function(lookupName, version, callback, scope) { this.invokeRemoteMethod(callback, scope, 'RegisterLookupDataVersion', [lookupName, version]);}
+});
+
+Ext.define('Dextop.Panel.remoting.Proxy', {
+	extend: 'Dextop.RemotableBase.remoting.Proxy'
 });
 
