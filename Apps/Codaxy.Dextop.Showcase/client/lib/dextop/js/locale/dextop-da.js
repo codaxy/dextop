a97f3046565e158427d15ba094bbcd1ee960f270
Igor T <igor.timarac@codaxy.com>
2011-11-25 15:34:43 +0100

    Danish translation

---
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/locale/dextop-da.js
@@ -0,0 +1,31 @@
+﻿/*
+ * Auto-generated file
+ * 11/25/2011 3:03:14 PM
+*/
+
+Ext.apply(Dextop, {
+	errorText: 'Fejl'
+	,warningText: 'Advarsel'
+	,confirmText: 'Bekræftelse'
+	,infoText: 'Information'
+	,saveText: 'Gem'
+	,cancelText: 'Annuler'
+	,editText: 'Rediger'
+	,addText: 'Tilføj'
+	,removeText: 'Fjern'
+	,reloadText: 'Genindlæs'
+});
+
+Ext.apply(Dextop.Session.prototype, {
+	sessionTerminatedReloadText: 'Din session er udløbet. Vil du starte en ny session?'
+});
+
+Ext.apply(Dextop.ux.FormEditorWindow.prototype, {
+	title: 'Rediger element'
+});
+
+Ext.apply(Dextop.ux.SwissArmyGrid.prototype, {
+	confirmDeleteText: 'Er du sikker på at du vil fjerne det valgte element?'
+	,pageSizeText: 'Størrelse: '
+});
+
