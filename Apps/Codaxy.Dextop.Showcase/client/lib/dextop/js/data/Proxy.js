16ced011987fdb4918aa90a572453241ec0b0432
Marko Stijak <mstijak@gmail.com>
2012-01-16 15:31:48 +0100

    total property for ext 4.1

--- Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/data/Proxy.js
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/data/Proxy.js
@@ -2,104 +2,105 @@ Ext.ns('Dextop.data');
 
 Ext.define('Dextop.data.Proxy', {
 
-    extend: 'Ext.data.ServerProxy',
+	extend: 'Ext.data.ServerProxy',
 
-    mixins: {
-        remotable: 'Dextop.remoting.Remotable'
-    },
+	mixins: {
+		remotable: 'Dextop.remoting.Remotable'
+	},
 
-    requires: ['Ext.data.ServerProxy', 'Dextop.remoting.Remotable'],
+	requires: ['Ext.data.ServerProxy', 'Dextop.remoting.Remotable'],
 
-    defaultWriterType: 'array',
-    defaultReaderType: 'array',
+	defaultWriterType: 'array',
+	defaultReaderType: 'array',
 
-    //callback options for remote method invocation
-    remoteCallbackOptions: undefined,
+	//callback options for remote method invocation
+	remoteCallbackOptions: undefined,
 
-    constructor: function (config) {
+	constructor: function (config) {
 
-        config = config || {};
+		config = config || {};
 
-        if (!config.reader || config.reader === 'array')
-            config.reader = Ext.create('Ext.data.ArrayReader', {
-                root: 'data'
-            });
+		if (!config.reader || config.reader === 'array')
+			config.reader = Ext.create('Ext.data.ArrayReader', {
+				root: 'data',
+				totalProperty: 'total'
+			});
 
-        if (config.remote) {
-            this.initRemote(config.remote);
-            delete config.remote;
-        }
+		if (config.remote) {
+			this.initRemote(config.remote);
+			delete config.remote;
+		}
 
-        this.callParent(arguments);
-    },
+		this.callParent(arguments);
+	},
 
-    applyEncoding: function (v) {
-        return v;
-    },
+	applyEncoding: function (v) {
+		return v;
+	},
 
-    doRequest: function (operation, callback, scope) {
-        var args = [];
+	doRequest: function (operation, callback, scope) {
+		var args = [];
 
-        switch (operation.action) {
-            case 'read':
-                var params = this.getParams(operation);
-                params.Params = Ext.apply(operation.params || {}, this.extraParams);
-                args.push(params);
-                break;
-            case 'create':
-            case 'update':
-            case 'destroy':
-                if (!this.writer)
-                    throw 'Proxy writer is not configured.';
-                var data = [];
-                for (var i = 0; i < operation.records.length; i++)
-                    data[i] = this.writer.getRecordData(operation.records[i]);
-                args.push(Ext.encode(data));
-                break;
-            default:
-                throw 'Uknown action ' + operation.action + '.';
-        }
-        args.push(this.createCallback(operation, callback, scope));
-        args.push(this);
-        this[operation.action + 'Record'].apply(this, args);
-    },
+		switch (operation.action) {
+			case 'read':
+				var params = this.getParams(operation);
+				params.Params = Ext.apply(operation.params || {}, this.extraParams);
+				args.push(params);
+				break;
+			case 'create':
+			case 'update':
+			case 'destroy':
+				if (!this.writer)
+					throw 'Proxy writer is not configured.';
+				var data = [];
+				for (var i = 0; i < operation.records.length; i++)
+					data[i] = this.writer.getRecordData(operation.records[i]);
+				args.push(Ext.encode(data));
+				break;
+			default:
+				throw 'Uknown action ' + operation.action + '.';
+		}
+		args.push(this.createCallback(operation, callback, scope));
+		args.push(this);
+		this[operation.action + 'Record'].apply(this, args);
+	},
 
-    createCallback: function (operation, callback, scope) {
-        return Ext.apply({
-            handler: function (r) {
-                this.processResponse(r.success, operation, null, r, callback, scope)
-            }
-        }, this.remoteCallbackOptions);
-    },
+	createCallback: function (operation, callback, scope) {
+		return Ext.apply({
+			handler: function (r) {
+				this.processResponse(r.success, operation, null, r, callback, scope)
+			}
+		}, this.remoteCallbackOptions);
+	},
 
-    extractResponseData: function (response) {
-        if (Ext.isString(response.result.data))
-            response.result.data = Ext.decode(response.result.data);
-        return response.result;
-    },
+	extractResponseData: function (response) {
+		if (Ext.isString(response.result.data))
+			response.result.data = Ext.decode(response.result.data);
+		return response.result;
+	},
 
-    //private
-    readRecord: function () {
-        this.remote.Load.apply(this.remote, arguments);
-    },
+	//private
+	readRecord: function () {
+		this.remote.Load.apply(this.remote, arguments);
+	},
 
-    //private
-    createRecord: function () {
-        this.remote.Create.apply(this.remote, arguments);
-    },
+	//private
+	createRecord: function () {
+		this.remote.Create.apply(this.remote, arguments);
+	},
 
-    //private
-    updateRecord: function () {
-        this.remote.Update.apply(this.remote, arguments);
-    },
+	//private
+	updateRecord: function () {
+		this.remote.Update.apply(this.remote, arguments);
+	},
 
-    //private
-    destroyRecord: function () {
-        this.remote.Destroy.apply(this.remote, arguments);
-    },
+	//private
+	destroyRecord: function () {
+		this.remote.Destroy.apply(this.remote, arguments);
+	},
 
-    onDestroy: function () {
-        this.destroyRemote();
-        this.callParent(arguments);
-    }
+	onDestroy: function () {
+		this.destroyRemote();
+		this.callParent(arguments);
+	}
 })
