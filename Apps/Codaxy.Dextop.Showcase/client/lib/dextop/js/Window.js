293b08db31cbe5c93e49e4828677ce3eacae0a19
Igor T <igor.timarac@codaxy.com>
2012-03-06 15:56:44 +0100

    DextopWindow state.

--- Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/Window.js
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/Window.js
@@ -1,7 +1,6 @@
 Ext.ns('Dextop');
 
 Ext.define('Dextop.Window', {
-
 	extend: 'Ext.window.Window',
 
 	mixins: {
@@ -10,6 +9,10 @@ Ext.define('Dextop.Window', {
 
 	requires: ['Ext.window.Window', 'Dextop.remoting.Remotable'],
 
+	// property
+	hasWindowState: false,
+	initialWindowState: null,
+		
 	constructor: function (config) {
 
 		if (config.remote) {
@@ -26,5 +29,24 @@ Ext.define('Dextop.Window', {
 		} catch (e) { }
 		this.destroyRemote();
 		this.callParent(arguments);
+	},
+
+	// virtual
+	getWindowState: function() {
+		if (!this.el) // never rendered, nothing could have changed
+			return this.initialWindowState
+			
+		// TODO When we have multiple pages, set up x, y and page index accordingly
+		var state = {
+			config: this.getBox(),
+			instantiateOptions: this.instantiateOptions
+		};
+
+		return state;
+	},
+
+	// virtual
+	restoreWindowState: function(state) {
+		this.initialWindowState = state;
 	}
 });
