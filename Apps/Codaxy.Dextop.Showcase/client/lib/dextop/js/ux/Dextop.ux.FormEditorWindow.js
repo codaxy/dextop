0ae71155d83e4cb6cb9789e718bf8bcbda686fa2
Boris Đurđević <djurdjevicboris@hotmail.com>
2012-08-15 15:01:21 +0200

    - readOnly option on FormEditorWindow

--- Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/ux/Dextop.ux.FormEditorWindow.js
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/ux/Dextop.ux.FormEditorWindow.js
@@ -1,64 +1,77 @@
 Ext.namespace('Dextop');
 
 Ext.define('Dextop.ux.FormEditorWindow', {
-	extend: 'Ext.Window',
+    extend: 'Ext.Window',
 
-	title: 'Edit Record',
+    title: 'Edit Record',
 
-	modal: true,
-	width: 400,
-	autoHeight: true,
+    modal: true,
+    width: 400,
+    autoHeight: true,
 
-	formConfig: undefined,
-	formItemsType: undefined,
-	formItemsConfig: undefined,
+    formConfig: undefined,
+    formItemsType: undefined,
+    formItemsConfig: undefined,
 
-	initComponent: function () {
+    readOnly: undefined,
 
-		this.addEvents(['save']);
+    initComponent: function () {
 
-		this.formConfig = this.formConfig || {};
+        this.addEvents(['save']);
 
-		if (!this.formItemsType)
-			throw 'FormEditor requires formItemsType property to be configured.';
+        this.formConfig = this.formConfig || {};
 
-		var items = Ext.create(this.formItemsType).getItems(Ext.apply({
-			remote: this.remote,
-			data: this.data
-		}, this.formItemsConfig));
+        if (!this.formItemsType)
+            throw 'FormEditor requires formItemsType property to be configured.';
 
-		this.form = Ext.create('Ext.form.Panel', Ext.applyIf(Ext.clone(this.formConfig), {
-			bodyStyle: 'padding: 5px',
-			border: false,
-			items: items
-		}));
+        var items = Ext.create(this.formItemsType).getItems(Ext.apply({
+            remote: this.remote,
+            data: this.data
+        }, this.formItemsConfig));
 
-		Ext.apply(this, {
-			layout: 'fit',			
-			items: [this.form],
-			buttons: [{
-				formBind: true,
-				text: Dextop.saveText,
-				scope: this,
-				handler: function () {
-					if (this.form.getForm().isValid()) {
-						var values = this.form.getForm().getFieldValues();
-						this.fireEvent('save', this, this.form, values);
-					}
-				}
-			}, {
-				text: Dextop.cancelText,
-				scope: this,
-				handler: function () {
-					this.close();
-				}
-			}]
-		});
+        if (this.readOnly)
+            this.applyReadOnlyOnItems(items);
 
-		this.callParent();
-	},
+        this.form = Ext.create('Ext.form.Panel', Ext.applyIf(Ext.clone(this.formConfig), {
+            bodyStyle: 'padding: 5px',
+            border: false,
+            items: items
+        }));
 
-	updateRecord: function (rec) {
-		this.form.getForm().updateRecord(rec);
-	}
+        Ext.apply(this, {
+            layout: 'fit',
+            items: [this.form],
+            buttons: [{
+                formBind: true,
+                text: Dextop.saveText,
+                scope: this,
+                handler: function () {
+                    if (this.form.getForm().isValid()) {
+                        var values = this.form.getForm().getFieldValues();
+                        this.fireEvent('save', this, this.form, values);
+                    }
+                }
+            }, {
+                text: Dextop.cancelText,
+                scope: this,
+                handler: function () {
+                    this.close();
+                }
+            }]
+        });
+
+        this.callParent();
+    },
+
+    applyReadOnlyOnItems: function (items) {
+        for (i = 0; i < items.length; i++) {
+            items[i].readOnly = true
+            if (Ext.isArray(items[i].items))
+                applyReadOnlyOnItems(items[i].items);
+        }
+    },
+
+    updateRecord: function (rec) {
+        this.form.getForm().updateRecord(rec);
+    }
 });
