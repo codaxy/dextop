f29d64d91ba3647f5a895c1ffe30d3d85579d645
Marko Stijak <mstijak@gmail.com>
2012-09-27 10:15:44 +0200

    Serbian language alternative date formats

--- Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/locale/ext-patch-sr.js
+++ Apps/Codaxy.Dextop.Showcase/client/lib/dextop/js/locale/ext-patch-sr.js
@@ -9,6 +9,11 @@
 			defaultFormat: 'd.m.Y'
 		});
 
+	Dextop.localize('Ext.form.field.Date', {
+		format: 'd.m.Y',
+		altFormats: "d.m.Y|j.n.Y|j.n.y|j.m.y|d.n.y|j.m.Y|d.m.Y|d-m-y|d-m-Y|d.m|d-m|dm|dmy|dmY|j|Y-n-j|j-n|j.n"
+	});
+
 	Dextop.localize('Ext.form.field.Number', {
 		decimalSeparator: ','
 	});
