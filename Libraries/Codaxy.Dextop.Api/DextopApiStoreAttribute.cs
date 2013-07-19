7b02b271a5d340e3c5b67f286883f08e27524711
Marko Stijak <marko.stijak@codaxy.com>
2013-07-19 10:16:21 +0200

    prepared stores

---
+++ Libraries/Codaxy.Dextop.Api/DextopApiStoreAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codaxy.Dextop.Api
+{
+    public class DextopApiStoreAttribute : System.Attribute
+    {
+        public DextopApiStoreAttribute(String storeId)
+        {
+            StoreId = storeId;
+        }
+
+        public string StoreId { get; set; }
+        
+        public bool autoLoad { get; set; }
+    }
+}
